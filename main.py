dc5027325a1f9eb451cb312ccb6b2378cb00f886
BrianWaldron97 <waldron97@yahoo.com>
2021-05-01 21:51:41 +0100

    Worked on main

--- main.py
+++ main.py
@@ -5,8 +5,11 @@
 import shunting
 import thompson
 
-# os allows for file path manipulation
+# os allows for file path manipulation (os.path)
 import os
+
+# sys allows for runtime manipulation
+#import sys
 
 # User enters directory of txt file
 userPath = input("Please enter the directory path: ")
@@ -29,7 +32,7 @@ if txtFileName:
             for line in f:
                 lineNumber += 1
                 if expressionSearch in line:
-                    results.append((lineNumber, line.rstrip()))
+                    results.append((lineNumber, line.rstrip())) # rstrip() removes trailing characters
         return results
     expressionsFound = expression_search(txtFileName, expressionSearch)
     print("Number of expressions found: ", len(expressionsFound))
@@ -38,3 +41,24 @@ if txtFileName:
 else:
     print("Sorry, " + txtFile + " does not exist in the directory given.")
     exit()
+
+
+if __name__ == "__main__":
+    tests = [
+        ["(a.b|b)", ["ab","b", "bb", "a"]],
+        ["a.(b.b).a", ["aa","abba", "aba"]],
+        ["1.(0.0)*.1", ["11","100001", "11001"]]
+    ]
+
+    for test in tests:
+        infix = test[0]
+        print(f"infix:      {infix}")
+        postfix = shunting.shunt(infix)
+        print(f"postfix:    {postfix}")
+        nfa = thompson.re_to_nfa(postfix)
+        print(f"thompson:   {nfa}")
+        for s in test[1]:
+            match = nfa.match(s)
+            print(f"Match '{s}': {match}")
+        print()
+

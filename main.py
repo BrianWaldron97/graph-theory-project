0fa8f9695f7545c635eaac0a1605ed4dab09658c
BrianWaldron97 <waldron97@yahoo.com>
2021-05-01 17:57:21 +0100

    Worked on main

--- main.py
+++ main.py
@@ -4,6 +4,7 @@
 
 import shunting
 import thompson
+
 # os allows for file path manipulation
 import os
 
@@ -37,22 +38,3 @@ if txtFileName:
 else:
     print("Sorry, " + txtFile + " does not exist in the directory given.")
     exit()
-
-
-# if __name__ == "__main__":
-#     tests = [  ["(a.b|b*)", ["ab", "b", "bb", "a"]]
-#              , ["a.(b.b)*.a", ["aa", "abba", "aba"]]
-#              , ["1.(0.0)*.1", ["11", "100001", "11001"]]
-#     ]
-
-#     for test in tests:
-#         infix = test[0]
-#         print(f"infix:    {infix}")
-#         postfix = shunting.shunt(infix)
-#         print(f"postfix:    {postfix}")
-#         nfa = thompson.re_to_nfa(postfix)
-#         print(f"thompson:    {nfa}")
-#         for s in test[1]:
-#             match = nfa.match(s)
-#             print(f"Match '{s}': {match}")
-#         print()
